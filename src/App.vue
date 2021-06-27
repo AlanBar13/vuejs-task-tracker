f6a57b9cad16f41bf4e3cddda400140c40d50ede
Alan Bardales <alan.g.bardales@gmail.com>
2021-06-27 01:14:30 -0500

    Added Button component

--- src/App.vue
+++ src/App.vue
@@ -38,4 +38,17 @@ body {
   border-radius: 5px;
 }
 
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
 </style>
