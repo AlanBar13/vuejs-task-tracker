23de09f09742140b9260c3faf0ec6a61f72d6d23
Alan Bardales <alan.g.bardales@gmail.com>
2021-06-29 12:40:50 -0500

    Added task view with delete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id){
+      if(confirm('Do you want to delete?')){
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:00am",
+        reminder: false
+      },
+    ]
   }
 }
 </script>
